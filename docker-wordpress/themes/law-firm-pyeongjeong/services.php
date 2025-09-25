2bbc380e3207af97cfee3641821d61d1578a787f
Evgenii <66017568+evgl@users.noreply.github.com>
2025-09-25 22:59:22 +0900

    Set a background for the services page

--- docker-wordpress/themes/law-firm-pyeongjeong/services.php
+++ docker-wordpress/themes/law-firm-pyeongjeong/services.php
@@ -22,7 +22,7 @@
     <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?> style="background-color: white;">
+<body <?php body_class('services-page'); ?>>
 <?php wp_body_open(); ?>
 
 <!-- Skip to main content for accessibility -->
@@ -75,10 +75,10 @@
 </header>
 
 <!-- Main Content Area -->
-<main id="main" class="site-main" role="main" style="background-color: white;">
+<main id="main" class="site-main" role="main">
 
 <!-- Blank Content Area -->
-<div style="min-height: 60vh; padding: 50px 0; background-color: white;">
+<div class="services-content">
     <!-- Content can be added here -->
 </div>
 
