d7726d917dae40801e357bc5243be9a8a9f007d6
evgl <evgenii.li@yahoo.com>
2026-05-08 16:54:27 +0900

    Align the design for hero in why-pjlaw page

--- docker-wordpress/themes/pjlaw/page-why-pjlaw.php
+++ docker-wordpress/themes/pjlaw/page-why-pjlaw.php
@@ -42,14 +42,16 @@ $theme_uri = get_template_directory_uri();
             </div>
 
             <div class="why-hero__footer">
-                <div class="why-hero__pager" aria-label="Breadcrumb">
-                    <a class="why-hero__pager-item why-hero__pager-item--home" href="<?php echo esc_url(home_url('/')); ?>">
-                        <img src="<?php echo esc_url($theme_uri . '/assets/images/why-pjlaw/icon-home.svg'); ?>" alt="" aria-hidden="true" class="why-hero__pager-home-icon" />
+                <div class="about-hero__pager" aria-label="About section navigation">
+                    <a class="about-hero__pager-item" href="<?php echo esc_url(home_url('/about/')); ?>">
                         <span>평정소개</span>
+                        <svg class="about-hero__pager-arrow" aria-hidden="true" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1 1L6 6L11 1" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
                     </a>
-                    <a class="why-hero__pager-item" href="<?php echo esc_url(home_url('/why-pjlaw/')); ?>">
+                    <a class="about-hero__pager-item" href="<?php echo esc_url(home_url('/why-pjlaw/')); ?>">
                         <span>왜 평정인가?</span>
-                        <svg class="why-hero__pager-arrow" aria-hidden="true" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg class="about-hero__pager-arrow" aria-hidden="true" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M1 1L6 6L11 1" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
                     </a>
