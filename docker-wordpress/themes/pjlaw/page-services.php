e758ac8220b536c52a1f788dcd40a18f5e1f8552
evgl <evgenii.li@yahoo.com>
2026-06-23 08:25:37 +0900

    Implemented go home and close post buttons in services

--- docker-wordpress/themes/pjlaw/page-services.php
+++ docker-wordpress/themes/pjlaw/page-services.php
@@ -115,8 +115,23 @@ if ($is_detail) {
     }
 }
 
-get_header();
 $theme_uri = get_template_directory_uri();
+
+if ($is_detail && $service_post) {
+    ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class('blog-post-page'); ?>>
+    <?php wp_body_open(); ?>
+    <?php
+} else {
+    get_header();
+}
 ?>
 
 <main id="main" class="site-main services-page" role="main">
